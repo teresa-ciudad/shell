13be380d1645cce594ec3c48e4e5c82bdd94ef1c
Teresa Ciudad <mtciugar@gmail.com>
2026-02-26 14:03:13 -0500

    Practice excercise during class

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+# I attempt to make a change
+
 ############################################
 # DSI CONSULTING INC. Project setup script #
 ############################################
